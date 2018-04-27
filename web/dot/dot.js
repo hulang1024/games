c6dbb82e362708ffd2c5853709f80e74e59de8bb
hulang1024 <1013644379@qq.com>
2018-04-27 22:21:49 +0800

    Add: 移动绘制

--- web/dot/dot.js
+++ web/dot/dot.js
@@ -46,36 +46,40 @@ function SImage() {
     this.width = 0;
     this.height = 0;
     /* [[r,g,b],...] */
-    this.data = []; 
+    this.data = [];
 }
 
 SImage.prototype.writeTextFile = function() {
   var text = '';
   text += this.width + ',';
   text += this.height + ',';
-  
+
   var len = this.data.length;
   this.data.forEach(function(rgba, index) {
     text += rgb2Hex(rgba);
     if (index < len - 1)
-      text += ',';    
+      text += ',';
   });
-  
+
   return text;
+}
+
+SImage.prototype.readFromText = function(text) {
+  var simage = this;
+  var ints = text.split(',');
+  simage.width = + ints.shift();
+  simage.height = + ints.shift();
+
+  ints.forEach(function(sn) {
+    simage.data.push( rgbHexValue2IntArray(sn) );
+  });
 }
 
 SImage.prototype.readFromTextFile = function(file, callback) {
   var reader = new FileReader();
   var simage = this;
   reader.onload = function() {
-    var ints = this.result.split(',');
-    simage.width = + ints.shift();
-    simage.height = + ints.shift();
-    
-    ints.forEach(function(sn) {
-      simage.data.push( rgbHexValue2IntArray(sn) );
-    });
-    
+    simage.readFromText(this.result);
     callback();
   };
   reader.readAsText(file);
@@ -87,29 +91,29 @@ function Board(params) {
   var board = this;
   var rowNum = params.rows;
   var colNum = params.cols;
-  var width = 500;
-  var height = 500;
+  var width = 600;
+  var height = 600;
   var gridEnabled = true;
   var DEFAULT_OUTLINE = '1px solid #dbdbdb';
-  
+
   var divBoard = $("#board");
   divBoard.css({
     width: width + 'px',
     height: height + 'px',
     margin: '0 auto'
   });
-  
+
   this.table = createTable();
-  
+
   divBoard.append(this.table);
-  
+
   this.signals = {
     mousedowned: new Signal(),
     mousemoved: new Signal(),
     mouseuped: new Signal(),
     clicked: new Signal()
   };
-  
+
   this.enableGrid = function(enable) {
     gridEnabled = enable;
     var outline = gridEnabled ? DEFAULT_OUTLINE: '';
@@ -117,7 +121,7 @@ function Board(params) {
       for (var c = 0; c < colNum; c++)
         $(this.table.rows[r].cells[c]).css('outline', outline);
   }
-  
+
   this.getSImage = function(data) {
     var data = [];
     for (var r = 0, rows = board.table.rows; r < rows.length; r++) {
@@ -125,15 +129,15 @@ function Board(params) {
         data.push( rgbValue2IntArray( $(rows[r].cells[c]).css('background-color') ) );
       }
     }
-    
+
     var simage = new SImage();
     simage.width = rowNum;
     simage.height = colNum;
     simage.data = data;
-    
+
     return simage;
   }
-  
+
   this.setSImage = function(simage) {
     var data = simage.data;
     var rows = this.table.rows;
@@ -143,7 +147,7 @@ function Board(params) {
       $(rows[r].cells[c]).css('background-color', '#' + rgb2Hex(data[i]));
     }
   }
-  
+
   function createTable() {
     var table = document.createElement('table');
     table.onmousedown = function(event) {
@@ -152,7 +156,7 @@ function Board(params) {
     table.onmouseup = function(event) {
       board.signals.mouseuped.dispatch(event);
     };
-    
+
     table = $(table);
     table.attr('cellpadding', 0);
     table.attr('cellspacing', 0);
@@ -161,7 +165,7 @@ function Board(params) {
       height: height + 'px',
       border: '2px solid black'
     });
-    
+
     var cellW = (width - (colNum-1)*2) / colNum;
     var cellH = (height - (rowNum-1)*2) / rowNum;
     var tr, td;
@@ -171,14 +175,14 @@ function Board(params) {
         td = document.createElement('td');
         td.position = {y: r, x: c};
         td.id = r + '-' + c;
-        
+
         td.onmousemove = function(event) {
           board.signals.mousemoved.dispatch(event);
         };
         td.onclick = function(event) {
           board.signals.clicked.dispatch(event);
         };
-        
+
         td = $(td);
         td.css({
           width: cellW + 'px',
@@ -191,10 +195,10 @@ function Board(params) {
       }
       table.append(tr);
     }
-    
+
     return table.get(0);
   }
-  
+
 }
 
 
@@ -203,15 +207,15 @@ function ColorSelector() {
   var nextIndex = 0;
   var COLOR_TABLE_ROWS = 3;
   var COLOR_TABLE_COLS = 10;
-  
+
   this.getRgbIntArray = function() {
     return rgbHexValue2IntArray(selectedRgb);
   }
-  
+
   this.getCode = function() {
     return '#' + selectedRgb;
   }
-  
+
   this.add = function(rgb) {
     if (nextIndex == COLOR_TABLE_COLS) {
       for (var i = 0; i < COLOR_TABLE_COLS-1; i++)
@@ -225,7 +229,7 @@ function ColorSelector() {
     $(cell).css('background-color', '#' + rgb);
     nextIndex++;
   }
-  
+
   var table = $('#colorTable').get(0);
   var tr, td;
   var cellSize = 20;
@@ -234,7 +238,7 @@ function ColorSelector() {
      'fff200', '22b14c', '00a2e8', '3f48cc', 'a349a4'],
     ['ffffff', 'c3c3c3', 'b97a57', 'ffaec9', 'ffc90e',
      'efe4b0', 'b5e61d', '99d9ea', '7092be', 'c8bfe7']];
-     
+
   for (var r = 0; r < COLOR_TABLE_ROWS; r++) {
     tr = document.createElement('tr');
     for (var c = 0; c < COLOR_TABLE_COLS; c++) {
@@ -260,12 +264,12 @@ function ColorSelector() {
         padding: 0,
         border: '1px solid #a0a0a0'
       });
-      
+
       tr.appendChild(td);
     }
     table.appendChild(tr);
   }
-  
+
   $('#selectedColorDisplay').css('background-color', '#' + selectedRgb);
 }
 
@@ -273,14 +277,14 @@ function ColorPicker() {
 }
 
 function Eraser() {
-   
+
 }
 
 function ColorFiller() {
   this.fill = function(color, position, simage) {
     fill(color, position.x, position.y, simage);
   };
-  
+
   function fill(color, x, y, simage) {
     if ((0 <= x && x < simage.width) && (0 <= y && y < simage.height)
       && isFillable(simage.data[y * simage.width + x])) {
@@ -291,7 +295,7 @@ function ColorFiller() {
       fill(color, x, y + 1, simage);
     }
   }
-  
+
   function isFillable(rgb) {
     return rgb.reduce(function(s, n){ return s + n; }, 0) >= 765;
   }
@@ -300,11 +304,11 @@ function ColorFiller() {
 function Selector(board) {
   var vec1 = null, vec2 = null;
   var lastRect;
-  
+
   this.signals = {
-    selected: new Signal()  
+    selected: new Signal()
   };
-  
+
   this.onClick = function(event) {
     if (vec1 == null) {
       vec1 = event.target.position;
@@ -321,30 +325,30 @@ function Selector(board) {
 
       setRangeRect(true);
       vec1 = vec2 = null;
-      
+
       this.signals.selected.dispatch(rect);
     }
   };
-  
+
   this.getRangeRect = function() {
     return lastRect;
   }
-  
+
   this.setRangeRect = setRangeRect;
-  
+
   function setRangeRect(b) {
     var rect = lastRect;
     if (!rect)
       return;
     showVec(vec1, b);
     showVec(vec2, b);
-    
+
     for (var y = 0; y < rect.height; y++)
       for (var x = 0; x < rect.width; x++)
         $(board.table.rows[rect.y + y].cells[rect.x + x]).css('outline',
           b ? '1px dashed black' : '1px solid #dbdbdb');
   }
-  
+
   function showVec(vec, b) {
     if (!vec)
       return;
@@ -356,24 +360,24 @@ function Selector(board) {
 function Clipboard() {
   var content = null;
   var isCut;
-  
+
   this.copyRange = function(srcRect) {
     content = srcRect;
     isCut = false;
   }
-  
+
   this.cutRange = function(srcRect) {
     content = srcRect;
     isCut = true;
   }
-  
+
   this.pasteRange = function(destRect, simage) {
     var srcRect = content;
     if (destRect.width < srcRect.width || destRect.height < srcRect.height) {
       alert('目标矩形尺寸小于源矩形尺寸');
       return;
     }
-    
+
     for (var y = 0; y < srcRect.height; y++)
       for (var x = 0; x < srcRect.width; x++)
         copyArray(
@@ -384,7 +388,7 @@ function Clipboard() {
       clearRange(srcRect, simage);
     }
   }
-  
+
   this.hasContent = function() {
     return content;
   }
@@ -425,33 +429,33 @@ function flipVertical(rect, simage) {
 function HistoryRecorder() {
   var stack = [];
   var top = -1;
-  
+
   this.signals = {
-    pushed: new Signal()  
+    pushed: new Signal()
   };
-  
+
   this.push = function(simage) {
     stack.push(simage);
     top++;
     this.signals.pushed.dispatch();
   }
-  
+
   this.isBottom = function() {
     return top == 0;
   }
   this.isTop = function() {
     return top == stack.length - 1;
   }
-  
+
   this.peek = function() {
     return stack[top];
   }
-  
+
   this.undo = function() {
     if (top > 0)
       top--;
   }
-  
+
   this.redo = function() {
     if (top <= stack.length - 1)
       top++;
@@ -459,48 +463,54 @@ function HistoryRecorder() {
 }
 
 $(function() {
-  var colorSelector = new ColorSelector();  
-    
+  var colorSelector = new ColorSelector();
+
   var historyRecorder = new HistoryRecorder();
 
   window.board = new Board({
     rows: 24,
     cols: 24
   });
-  
+
   historyRecorder.push(board.getSImage());
-  
+
   var selector = new Selector(board);
   var eraser = new Eraser();
   var colorFiller = new ColorFiller();
   var clipboard = new Clipboard();
   var nowTool;
-  
+
   board.signals.mousemoved.add(function(event) {
     var position = event.target.position;
+    if (event.buttons == 1) {
+      if ((nowTool instanceof Selector) || (nowTool instanceof ColorFiller)) {
+        return;
+      }
+      var cell = event.target;
+      if (nowTool instanceof Eraser) {
+        $(cell).css('background-color', 'transparent');
+        historyRecorder.push(board.getSImage());
+      } else {
+        $(cell).css('background-color', colorSelector.getCode());
+        historyRecorder.push(board.getSImage());
+      }
+    }
     $('#position').text('X:' + position.x + ', ' + 'Y:' + position.y);
   });
-  
+
   board.signals.clicked.add(function(event) {
     var cell = event.target;
-    
-    var nowSelector = nowTool instanceof Selector;
-    if (!nowSelector)
+
+    if (!(nowTool instanceof Selector))
       selector.setRangeRect(false);
-  
-    if (nowSelector) {
+
+    if (nowTool instanceof Selector) {
       selector.onClick(event);
-    } else if (nowTool instanceof Eraser) {
-      $(cell).css('background-color', 'transparent');
-      historyRecorder.push(board.getSImage());
     } else if (nowTool instanceof ColorFiller) {
       var simage = board.getSImage();
       colorFiller.fill(colorSelector.getRgbIntArray(), cell.position, simage);
       board.setSImage(simage);
       historyRecorder.push(simage);
-    } else {
-      $(cell).css('background-color', colorSelector.getCode());
-      historyRecorder.push(board.getSImage());
     }
   });
 
@@ -509,12 +519,12 @@ $(function() {
     if (clipboard.hasContent())
       $('#paste').prop('disabled', false);
   });
-  
+
   historyRecorder.signals.pushed.add(function(){
     $('#undo').prop('disabled', false);
     $('#redo').prop('disabled', true);
   });
-  
+
   $('#undo').click(function() {
     historyRecorder.undo();
     board.setSImage(historyRecorder.peek());
@@ -524,7 +534,7 @@ $(function() {
       $('#redo').prop('disabled', false);
     }
   }).prop('disabled', true);
-  
+
   $('#redo').click(function() {
     historyRecorder.redo();
     board.setSImage(historyRecorder.peek());
@@ -533,7 +543,7 @@ $(function() {
       $('#undo').prop('disabled', false);
     }
   }).prop('disabled', true);
-  
+
   $('#flipHorizontal').click(function() {
     var rect = selector.getRangeRect();
     var simage = board.getSImage();
@@ -541,7 +551,7 @@ $(function() {
     board.setSImage(simage);
     historyRecorder.push(simage);
   }).prop('disabled', true);
-  
+
   $('#flipVertical').click(function() {
     var rect = selector.getRangeRect();
     var simage = board.getSImage();
@@ -549,12 +559,12 @@ $(function() {
     board.setSImage(simage);
     historyRecorder.push(simage);
   }).prop('disabled', true);
-  
+
   $('#copy').click(function() {
     var srcRect = selector.getRangeRect();
     clipboard.copyRange(srcRect);
   }).prop('disabled', true);
-  
+
   $('#paste').click(function() {
     var destRect = selector.getRangeRect();
     var simage = board.getSImage();
@@ -562,12 +572,12 @@ $(function() {
     board.setSImage(simage);
     historyRecorder.push(simage);
   }).prop('disabled', true);
-  
+
   $('#cut').click(function() {
     var srcRect = selector.getRangeRect();
     clipboard.cutRange(srcRect);
   }).prop('disabled', true);
-  
+
   $('#clear').click(function() {
     var rect = selector.getRangeRect();
     var simage = board.getSImage();
@@ -575,54 +585,70 @@ $(function() {
     board.setSImage(simage);
     historyRecorder.push(simage);
   }).prop('disabled', true);
-  
+
   $('#selector').change(function() {
-    nowTool = this.checked ? selector : null;
-    if (!this.checked) {
+    if (this.checked) {
+      nowTool = selector;
+      setCursorInBoard('selector');
+    } else {
+      nowTool = null;
+      setCursorInBoard(null);
       selector.setRangeRect(false);
       $('#copy,#cut,#paste,#clear,#flipHorizontal,#flipVertical').prop('disabled', true);
     }
     $('#colorFiller,#eraser').prop('checked', false);
   }).prop('checked', false);
-  
+
   $('#eraser').change(function() {
-    nowTool = this.checked ? eraser : null;
+    if (this.checked) {
+      nowTool = eraser;
+      setCursorInBoard('eraser');
+    } else {
+      nowTool = null;
+      setCursorInBoard(null);
+    }
     $('#colorFiller,#selector').prop('checked', false);
   }).prop('checked', false);
-  
+
   $('#colorFiller').change(function() {
-    nowTool = this.checked ? colorFiller : null;
+    if (this.checked) {
+      nowTool = colorFiller;
+      setCursorInBoard('colorFiller');
+    } else {
+      nowTool = null;
+      setCursorInBoard(null);
+    }
     $('#eraser,#selector').prop('checked', false);
   }).prop('checked', false);
-  
+
   $('#toggleGridEnable').change(function() {
     board.enableGrid(this.checked);
   });
-  
+
   $('#editColor').click(function() {
-    $('#colorEditor').toggle(); 
+    $('#colorEditor').toggle();
   });
-  
+
   $('#addToCustomList').click(function() {
     colorSelector.add($('#rgbHex').val());
   });
 
   $('#save').click(function(){
     var formatters = [];
-  
+
     formatters['default'] = function(simage) {
       return simage.writeTextFile();
     };
-  
+
     formatters['columns'] = function(simage) {
       return simage.data.map(function(rgb) {
         return rgb.join(',');
       }).join('\n');
     };
-    
+
     var formatName = $('#selFormat').val() || 'default';
     var formatter = formatters[formatName];
-    
+
     var simage = board.getSImage();
     var output = formatter(simage);
 
@@ -632,8 +658,8 @@ $(function() {
     window.open( objectURL, '_blank' );
     window.focus();
   });
-  
-  $('#open').click(function(){ 
+
+  $('#open').click(function(){
     var fileUpload = document.getElementById('file');
     if(fileUpload == null) {
         fileUpload = document.createElement('input');
@@ -655,4 +681,18 @@ $(function() {
 
     fileUpload.click();
   });
+
+  function setCursorInBoard(toolName) {
+    $('#board').css('cursor', ({
+      null: 'default',
+      'selector': 'crosshair',
+      'eraser': 'pointer',
+      'colorFiller': 'default'
+    })[toolName]);
+  }
+
+  // 初始化一个例子
+  var simage = new SImage();
+  simage.readFromText($('#ji_simage_data').text());
+  board.setSImage(simage);
 });
